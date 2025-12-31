cc63eed4235070db2993c7331da3747fb70a3095
okjo <soldung12@gmail.com>
2025-12-31 10:01:59 +0900

    feat : 로그인 API 연동 1차 구현

--- web/js/signin.js
+++ web/js/signin.js
@@ -6,12 +6,16 @@ signinForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const formData = new FormData(signinForm);
-  const username = formData.get("username");
-  const password = formData.get("password");
-  const role = formData.get("role");
+  const username = (formData.get("username") || "").trim();
+  const password = (formData.get("password") || "").trim();
+
+  const API_BASE_URL = 
+  window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    ? "http://localhost:3000/api"  // 로컬 개발
+    : "https://open-market-jade.vercel.app/api";  // Vercel 배포
 
   try {
-    const response = await fetch("/api/accounts/signin", {
+    const response = await fetch(`${API_BASE_URL}/accounts/signin`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -19,18 +23,25 @@ signinForm.addEventListener("submit", async (event) => {
       body: JSON.stringify({
         username,
         password,
-        // role,
       }),
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      localStorage.setItem("token", data.access);
-      window.location.href = "/html/main.html";
+    const data = await response.json();
+
+    if (response.ok) {   
+      localStorage.setItem("access_token", data.access);
+      localStorage.setItem("refresh_token", data.refresh);
+      localStorage.setItem("user", JSON.stringify(data.user));
+      
+      // window.location.href = "/html/index.html";
+      alert("로그인 성공!");
+
     } else {
-      alert("로그인 실패");
+     alert(data.message || data.error || data.detail || "로그인 실패");
     }
   } catch (error) {
     console.error("Error:", error);
   }
 });
+
+
