19136abade303bb8a5d4f43aa4f5d9a8055bd9c9
www1240kr-web <www.1240.kr@gmail.com>
2026-01-04 03:25:34 +0900

    feat: 로그인 모달 추가

--- web/js/detail.js
+++ web/js/detail.js
@@ -1,1 +1,22 @@
 // 상품 상세
+
+// 모달 관련 스크립트
+const modal = document.getElementById("modal-login");
+const openModal = document.getElementById("btn-cart");
+const closeModal = [
+  document.getElementById("close-btn"),
+  document.getElementById("btn-no"),
+];
+
+// 장바구니에 상품 추가 모달 열기
+openModal.addEventListener("click", function (event) {
+  event.preventDefault();
+  modal.classList.remove("hidden");
+});
+// 모달 닫기
+closeModal.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    modal.classList.add("hidden");
+  });
+});
